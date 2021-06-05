24d12f274d35d93076bd3e079996b58e59a1d1b1
Ferxis7577 <78823748+Ferxis7577@users.noreply.github.com>
2021-06-04 22:33:18 -0500

    ejercicio3

---
+++ Parcial3/Ejercicio3/datagrid.js
@@ -0,0 +1,18 @@
+$('document').ready(function(){
+    $.post('datagrid.php', function(resultados){
+        $('#datagrid').jsGrid({
+            width: "100%", height: "400px", editing:true, sorting: true,
+            paging:true,pageIndex:1, pageSize:10,  loadIndication: true,
+            loadIndicationDelay: 500, confirmDeleting: true, deleteConfirm: "Favor de confirmar",
+
+             // si se requiere tener un paginado, se activa pagePrevText:"Last",  pageNextText:"First",
+
+            data: resultados,
+            fields: [{name: "idusuario", type: "text", title: "idUsuario", width: 15, validate:"requiered", align: "center"},
+                    {name:"nombreusuario", type:"text", title:"NombreUsuario", width:30,align: "left"},
+                    {name:"correo", type:"text", title:"Correo", width:40,align: "left"},
+                    {name:"pswrd", type:"number", title:"Password", width:70,align: "left"},
+        ]
+        });
+    },'json');
+});
