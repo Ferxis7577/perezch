ef398165ae77ed2e88e65553a62d315fbc95fc48
Ferxis7577 <78823748+Ferxis7577@users.noreply.github.com>
2021-06-07 17:18:12 -0500

    commit

--- Parcial3/LoginBD_ABC/php/editar.php
+++ Parcial3/LoginBD_ABC/php/editar.php
@@ -1,41 +1,49 @@
 
-
 <?php
 
 if(isset($_GET['editar'])){
 	$editar_id = $_GET['editar'];
 
-	$consulta = "SELECT * FROM usuarios WHERE idusuario = 'editar_id'";
+	$consulta = "SELECT * FROM usuarios WHERE idusuario =" .$editar_id;
 	$ejecutar = mysqli_query($con, $consulta);
 	$fila = mysqli_fetch_array($ejecutar);
+	$idusuario=$fila['idusuario'];
 	$usuario = $fila['nombreusuario'];
 	$email = $fila['correo'];
 	$password = $fila['pswrd'];
 }
 ?>
 <br />
-<form method="POST" action="">
+<form method="GET" action="#">
+<input type="text" name="idusuario" value="<?php echo $idusuario; ?>" ><br />
 <input type="text" name="nombre" value="<?php echo $usuario; ?>"><br />
 <input type="text" name="email" value="<?php echo $email; ?>"><br />
 <input type="password" name="passw" value="<?php echo $password; ?>"><br />
-<input type="submiT" name="actualizar" value="ACTUALIZAR DATOS">
+<input type="submit" name="actualizar" value="ACTUALIZAR DATOS">
 </form>
 
 <?php
-if (isset($_POST['actualizar'])){
-$actualizar_nombre = $_POST['nombre'];
-$actualizar_email = $_POST['email'];
-$actualizar_password = $_POST['passw'];
+if (isset($_GET['actualizar'])){
+
+$actualizar_id = $_GET['idusuario'];	
+$actualizar_nombre = $_GET['nombre'];
+$actualizar_email = $_GET['email'];
+$actualizar_password = $_GET['passw'];
 
 $actualizar_password=password_hash($actualizar_password,PASSWORD_DEFAULT);
 
 
-$actualizar = "UPDATE usuarios SET nombreusuario='$actualizar_nombre', password='$actualizar_password', email='$actualizar_email' WHERE id='$editar_id'";
+$actualizar = "UPDATE usuarios SET nombreusuario='$actualizar_nombre', pswrd='$actualizar_password', email='$actualizar_email' WHERE idusuario= '$actualizar_id'";
 $ejecutar = mysqli_query($con, $actualizar);
+echo $ejecutar;
 
 if ($ejecutar){
+	
 	echo "<script>alert('Datos Actualizados!')</script>";
-	echo "<script>windoows.open('ABC.php','_self')</script>";
+	echo "<script>windows.open('ABC.php','_self')</script>";
+}
+else{
+	echo "asdasdas";
 }
 }
 ?>
